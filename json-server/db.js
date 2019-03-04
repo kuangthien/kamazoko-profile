e42c3872c52dfe889186fb8660ec48233e9fa275
Tran Kuang Thien <kuangthien@gmail.com>
2019-03-05 00:24:27 +0700

    mock data + intergate api to ui

--- json-server/db.js
+++ json-server/db.js
@@ -1,7 +1,21 @@
 // index.js
 
 module.exports = () => {
-	const data = { test: [] };
+	const data = {
+		profile: {
+			storeImg: 'dddhttps://loremflickr.com/320/240?random=1',
+			store: {
+				name: 'name',
+				address: 'address',
+				phone: 'phone',
+			},
+			redInvoice: {
+				companyName: 'companyName',
+				address: 'address',
+				mst: 'mst',
+			},
+		},
+	};
 
 	return data;
 };
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,60 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+const url = process.env.REACT_APP_API_URL;
 
-const Profile = () => {
-	return (
-		<div className='card'>
-			<img
-				className='card-img-top'
-				src='https://loremflickr.com/320/240?random=1'
-				alt='Card image cap'
-			/>
-			<div className='card-body'>
-				<div className='card-text'>
-					<table className='text-muted table   table-borderless table-sm'>
-						<tr className='text-uppercase'>
-							<th colSpan={2}>Store Info</th>
-						</tr>
-						<tr>
-							<td>Name</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td>Address</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td>Phone #</td>
-							<td>&nbsp;</td>
-						</tr>
-					</table>
+class Profile extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			profile: null,
+		};
+	}
+	componentDidMount() {
+		axios
+			.get(`${url}/profile`)
+			.then(rs => {
+				this.setState({
+					profile: rs.data,
+				});
+			})
+			.catch(e => e);
+	}
+	render() {
+		const { profile } = this.state;
+		if (!profile) return null;
+
+		return (
+			<div className='card'>
+				<img
+					className='card-img-top'
+					src='https://loremflickr.com/320/240?random=1'
+					alt='Card image cap'
+				/>
+				<div className='card-body'>
+					<div className='card-text'>
+						<table className='text-muted table   table-borderless table-sm'>
+							<tbody>
+								<tr className='text-uppercase'>
+									<th colSpan={2}>Store Info</th>
+								</tr>
+								<tr>
+									<td>Name</td>
+									<td>{profile.store.name}</td>
+								</tr>
+								<tr>
+									<td>Address</td>
+									<td>{profile.store.address}</td>
+								</tr>
+								<tr>
+									<td>Phone #</td>
+									<td>{profile.store.phone}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+
+					<div className='card-text'>
+						<table className=' text-muted table   table-borderless  table-sm'>
+							<tbody>
+								<tr className='text-uppercase'>
+									<th colSpan={2}>Red Invoice Info</th>
+								</tr>
+								<tr>
+									<td>Company Name</td>
+									<td>{profile.redInvoice.companyName}</td>
+								</tr>
+								<tr>
+									<td>Address</td>
+									<td>{profile.redInvoice.address}</td>
+								</tr>
+								<tr>
+									<td>MST</td>
+									<td>{profile.redInvoice.mst}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+
+					<a href='/' className='mt-2 btn btn-secondary '>
+						Edit Profile
+					</a>
 				</div>
-
-				<div className='card-text'>
-					<table className=' text-muted table   table-borderless  table-sm'>
-						<tr className='text-uppercase'>
-							<th colSpan={2}>Red Invoice Info</th>
-						</tr>
-						<tr>
-							<td>Company Name</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td>Address</td>
-							<td>&nbsp;</td>
-						</tr>
-						<tr>
-							<td>MST</td>
-							<td>&nbsp;</td>
-						</tr>
-					</table>
-				</div>
-
-				<a href='/' className='mt-2 btn btn-secondary '>
-					Edit Profile
-				</a>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default Profile;
