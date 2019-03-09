21f36e3ad879f888f4358ad534e16b0e87c88dd2
Tran Kuang Thien <kuangthien@gmail.com>
2019-03-09 19:17:30 +0700

    no message

--- src/components/ProfileEditModal.js
+++ src/components/ProfileEditModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProfileEditModal = (props) => {
+const ProfileEditModal = props => {
 	return (
 		<div className='col-12'>
 			<div className='row'>
@@ -9,7 +9,8 @@ const ProfileEditModal = (props) => {
 						<img
 							className='card-img-top'
 							src='https://loremflickr.com/320/240?random=1'
-							alt='Card image cap'
+                            alt='Card image cap'
+                            style={{width:'100%', minWidth:160}}
 						/>
 						<p className='text-right  mt-2'>
 							<button className='btn btn-sm btn-secondary'>
@@ -23,35 +24,54 @@ const ProfileEditModal = (props) => {
 						<h3>Store Info</h3>
 						<div className='mb-3'>
 							<label>Store Name</label>
-							<input className='form-control' />
+							<input
+								className='form-control'
+								placeholder='Store Name'
+							/>
 						</div>
 
 						<div className='mb-3'>
 							<label>Store Add</label>
-							<input className='form-control' />
+							<input
+								className='form-control'
+								placeholder='Store Add'
+							/>
 						</div>
 
 						<div className='mb-3'>
 							<label>Store Phone</label>
-							<input className='form-control' />
+							<input
+								className='form-control'
+								placeholder='Store Phone'
+							/>
 						</div>
 
 						<h3>Red Invoice Info</h3>
 
 						<div className='mb-3'>
 							<label>Company Name</label>
-							<input className='form-control' />
+							<input
+								className='form-control'
+								placeholder='Company Name'
+							/>
 						</div>
 
 						<div className='mb-3'>
 							<label>Company Add</label>
-							<input className='form-control' />
+							<input
+								className='form-control'
+								placeholder='Company Add'
+							/>
 						</div>
 
 						<div className='mb-3'>
 							<label>Company Phone</label>
-							<input className='form-control' />
+							<input
+								className='form-control'
+								placeholder='Company Phone'
+							/>
 						</div>
+                        <hr/>
 						<button
 							className='btn btn-primary btn-lg btn-block'
 							type='submit'
@@ -59,7 +79,7 @@ const ProfileEditModal = (props) => {
 							Save
 						</button>
 						<button
-							className='btn'
+							className='btn btn-secondary btn-lg btn-block'
 							onClick={props.handleClose}
 						>
 							Cancel
